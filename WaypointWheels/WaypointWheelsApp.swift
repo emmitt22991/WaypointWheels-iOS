222faccfd99ecc7b77104d346f558bdf29cbbaaf
emmitt22991 <dan@stepstonetexas.com>
2025-10-09 07:36:35 -0500

    Initial Commit

---
+++ WaypointWheels/WaypointWheelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaypointWheelsApp.swift
+//  WaypointWheels
+//
+//  Created by Daniel Francis on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WaypointWheelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaypointWheelsTests/WaypointWheelsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaypointWheelsTests.swift
+//  WaypointWheelsTests
+//
+//  Created by Daniel Francis on 10/9/25.
+//
+
+import Testing
+@testable import WaypointWheels
+
+struct WaypointWheelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
